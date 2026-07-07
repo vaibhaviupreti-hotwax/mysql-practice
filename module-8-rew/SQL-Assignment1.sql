ee8a89a2f5af1f911fa0c6e20918077c3064da22
Vaibhavi Upreti <vaibhavi.upreti@hotwax.co>
2026-07-07 15:01:10 +0530

    added assignment -1 mysql - raw queries

--- module-8-rew/SQL-Assignment1.sql
+++ module-8-rew/SQL-Assignment1.sql
@@ -139,6 +139,7 @@ WHERE
   STATUS_DATETIME <= '2026-03-31' 
   AND STATUS_DATETIME >= '2026-03-01' 
   AND OI.STATUS_ID = 'ITEM_COMPLETED'
+--------------------------------------------------------------------------------------------------------
 
 -- 6 Newly Created Sales Orders and Payment Methods
 -- Business Problem:
@@ -160,5 +161,121 @@ FROM
   ORDER_HEADER OH 
   JOIN SHOPIFY_SHOP_ORDER SSO ON OH.ORDER_ID = SSO.ORDER_ID 
   JOIN order_payment_preference OPP ON OH.ORDER_ID = OPP.ORDER_ID
+
+-- 8 Payment Captured but Not Shipped
+-- Business Problem:
+-- Finance teams want to ensure revenue is recognized properly. If payment is captured but no shipment has occurred, it warrants further review.
+
+-- Fields to Retrieve:
+
+-- ORDER_ID
+-- ORDER_STATUS
+-- PAYMENT_STATUS
+-- SHIPMENT_STATUS
+
+SELECT 
+  SS.STATUS_ID, 
+  OH.ORDER_ID, 
+  OH.STATUS_ID, 
+  -- OPP.STATUS_ID
+  -- DISTINCT OH.STATUS_ID
+FROM 
+  ORDER_HEADER OH 
+  JOIN ORDER_SHIPMENT OS ON OH.ORDER_ID = OS.ORDER_ID 
+  JOIN SHIPMENT_STATUS SS ON OS.SHIPMENT_ID = SS.SHIPMENT_ID 
+  AND SS.STATUS_ID <> 'SHIPMENT_SHIPPED' 
+  JOIN ORDER_PAYMENT_PREFERENCE OPP ON OPP.ORDER_ID = OH.ORDER_ID 
+  AND OPP.STATUS_ID = 'PAYMENT_SETTLED'
+
+-- 9 Orders Completed Hourly
+-- Business Problem:
+-- Operations teams want to see the volume of completed orders by hour to manage staffing and logistics.
+
+-- Fields to Retrieve:
+-- TOTAL ORDERS
+-- HOUR
+
+SELECT HOUR(STATUS_DATETIME) AS GRP_HOURS, COUNT(DISTINCT ORDER_ID) AS ORDERS
+from order_status  
+WHERE STATUS_ID='ORDER_COMPLETED'
+GROUP BY HOUR(STATUS_DATETIME)
+ORDER BY GRP_HOURS
+
+-- 10 BOPIS Orders Revenue (Last Year)
+-- Business Problem:
+-- BOPIS (Buy Online, Pickup In Store) is a key retail strategy. Finance wants to know the revenue from BOPIS orders for the previous year.
+
+-- Fields to Retrieve:
+
+-- TOTAL ORDERS
+-- TOTAL REVENUE
+
+select 
+  OISG.SHIPMENT_METHOD_TYPE_ID AS BOPIS_orders, 
+  SUM(OH.GRAND_TOTAL) AS TOT_REVENUE 
+from 
+  order_header OH 
+  JOIN order_item_ship_group OISG ON OH.ORDER_ID = OISG.ORDER_ID 
+  AND OH.ORDER_TYPE_ID = 'SALES_ORDER' 
+  --BUY ONLINE_(BOPIS-1)
+WHERE 
+  OISG.SHIPMENT_METHOD_TYPE_ID = 'STOREPICKUP' AND   YEAR(OH.ORDER_DATE)= YEAR(NOW())-1
+  --PICKUP IN STORE_(BOPIS-2)
+GROUP BY 
+  OISG.SHIPMENT_METHOD_TYPE_ID 
+
+--11 Canceled Orders (Last Month)
+-- Business Problem:
+-- The merchandising team needs to know how many orders were canceled in the previous month and their reasons.
+
+-- Fields to Retrieve:
+
+-- TOTAL ORDERS
+-- CANCELATION REASON
+
+SELECT * 
+-- COUNT(OS.ORDER_ID)
+-- OS.ORDER_ID,OS.STATUS_ID,OS.CHANGE_REASON, OS.CHANGE_REASON_ENUM_ID 
+FROM 
+ORDER_STATUS OS 
+WHERE OS.STATUS_ID = 'ORDER_CANCELLED' AND MONTH(STATUS_DATETIME)=MONTH(NOW())-1
+----------------------------------------------------
+SELECT 
+  COUNT(OS.ORDER_ID), 
+  OS.ORDER_ID, 
+  OS.STATUS_ID, 
+  OS.CHANGE_REASON, 
+  OS.CHANGE_REASON_ENUM_ID 
+FROM 
+  ORDER_STATUS OS 
+WHERE 
+  OS.STATUS_ID = 'ORDER_CANCELLED' 
+  AND MONTH(STATUS_DATETIME) = INTERVAL 1 MONTH;
+
+-- 12 Product Threshold Value
+-- Business Problem The retailer has set a threshild value for products that are sold online, in order to avoid over selling.
+
+-- Fields to Retrieve:
+
+-- PRODUCT ID
+-- THRESHOLD
+
+SELECT * from product_facility where MINIMUM_STOCK > 0 ;
+
+SELECT PF.PRODUCT_ID,
+PF.MINIMUM_STOCK
+FROM product_facility PF --CHECK ONLINE SELLABLE
+JOIN INVENTORY_ITEM INI
+WHERE
+INI.AVAILABLE_TO_PROMISE > 0
+-- INVENTORY_ITEM_TYPE_ID=""
+
+
+
+
+
+
+
+
 
 
